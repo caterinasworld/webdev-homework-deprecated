927d6979bb6c42c5e5e51ade70b73e548001623c
Caterina Paun <caterina.paun@gmail.com>
2021-07-25 23:09:18 -0700

    Update: starter files for homework 4

---
+++ hw4/03-age.js
@@ -0,0 +1,15 @@
+const catculateAge = (birthday) => {
+  // Given a date, return the age of the person with that birthday, in years, months, and days
+};
+
+// Date formats
+// new Date('December 17, 1995')
+// new Date('1995-12-17')
+// new Date(1995, 11, 17)
+// new Date(628021800000)
+
+console.log(catculateAge(birthday));
+// Age: 25 years, 4 months, 3 days
+// Age: 25 years, 1 day
+// Age: 4 months, 10 days
+// Error: invalid input provided
