1f24b6186a59417ea7797244b4504887360daeef
Caterina Paun <caterina.paun@gmail.com>
2021-08-03 02:03:54 -0700

    Add: starter files for homework 5

---
+++ hw5/02-beverages.js
@@ -0,0 +1,10 @@
+const url =
+  'https://www.thecocktaildb.com/api/json/v1/1/filter.php?a=Non_Alcoholic';
+
+let app = document.querySelector('#results');
+
+const fetchData = (url) => {
+  // Add your code here
+};
+
+fetchData(url);
